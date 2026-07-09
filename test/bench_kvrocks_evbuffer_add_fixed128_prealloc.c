2a0f20a857bf72286019773761ae65afc638c5e9
shixianjie <shixianjie1@huawei.com>
2026-07-09 09:37:53 +0800

    bench: add evbuffer_add fixed128_prealloc static-link variant

--- test/bench_kvrocks_evbuffer_add_fixed128_prealloc.c
+++ test/bench_kvrocks_evbuffer_add_fixed128_prealloc.c
@@ -23,6 +23,9 @@
 #define DEFAULT_DURATION 3
 #define DEFAULT_VALUE_SIZE 128
 #define BATCH_OPS 4096
+#ifndef BENCH_NAME
+#define BENCH_NAME "evbuffer_add_fixed128_prealloc"
+#endif
 
 #ifdef _WIN32
 #define U64_FMT "%I64u"
@@ -135,7 +138,7 @@ main(int argc, char **argv)
 			break;
 	}
 
-	printf("bench=evbuffer_add_fixed128_prealloc ns_per_op=%.2f\n",
+	printf("bench=%s ns_per_op=%.2f\n", BENCH_NAME,
 	    done ? (double)total_usec * 1000.0 / done : 0.0);
 
 	free(value);
